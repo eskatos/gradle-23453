d33378925dc8d7dff5918d5091d7f4a63b53a2f6
Paul Merlin <paul@gradle.com>
2023-01-10 15:55:05 +0100

    add specifics from issue op

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,19 @@ plugins {
     `kotlin-dsl`
 }
 
+dependencies {
+    val kotlinVer = "1.8.0"
+    implementation(platform(kotlin("bom", kotlinVer)))
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVer")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:$kotlinVer")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of("11"))
+    }
+}
+
 repositories {
     mavenCentral()
 }
